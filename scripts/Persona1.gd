16403e39e98831a57edd57768f4d462889f00dc7
OhEsPaco <fcomanuel.garcia4@alu.uclm.es>
2020-04-11 22:09:23 +0200

    Añadido techo

--- scripts/Persona1.gd
+++ scripts/Persona1.gd
@@ -13,21 +13,29 @@ var timeBetweenImpulses_seconds=2
 func _ready():
 	player=get_parent().get_parent().get_node("Jugador")
 	timeBetweenImpulses_seconds=timeBetweenImpulses_seconds+timeBetweenImpulses_seconds*rand_range(0,1)
+	var initialImpulse=Vector3(rand_range(0,1),rand_range(0,1),rand_range(0,1))
+	initialImpulse=initialImpulse.normalized()*rand_range(SPEED/2,SPEED)
+	apply_central_impulse(initialImpulse)
 	
 func _physics_process(delta):
 	if currentTimeBetweenImpulses>=timeBetweenImpulses_seconds:
 		currentTimeBetweenImpulses=delta
-		
-		var playerPos=player.translation
-		var myPos=translation
+		if(rand_range(0,1)<0.1):
+			var randomImpulse=Vector3(rand_range(0,1),rand_range(0,1),rand_range(0,1))
+			randomImpulse=randomImpulse.normalized()*rand_range(SPEED/2,SPEED)
+			linear_velocity=Vector3()
+			apply_central_impulse(randomImpulse)
+		else:
+			var playerPos=player.translation
+			var myPos=translation
 	
-		var direction=playerPos-myPos
-		direction=direction.normalized()
+			var direction=playerPos-myPos
+			direction=direction.normalized()
 	
-		var target=direction*SPEED
-		linear_velocity=Vector3()
-		
-		apply_central_impulse(target)
+			var target=direction*SPEED
+			linear_velocity=Vector3()	
+			
+			apply_central_impulse(target)
 	else:
 		currentTimeBetweenImpulses=currentTimeBetweenImpulses+delta
 
